aa4da42dd1e1ec4ca46dfb1e86da175aa580f425
DzmitryGrom <Shelegeykodmitriy@mail.ru>
2016-05-15 00:15:28 +0300

    add html,js,css tabs

---
+++ myjs.js
@@ -0,0 +1,28 @@
+(function(){
+ 
+  box.addEventListener('click', function(event){
+    var target = event.target;
+    list = document.querySelectorAll('li');
+    textBoxs = document.querySelectorAll('.container-caption-text');
+      console.log(typeof target.parentElement.id)
+      for(var i = 0; i < list.length; i++){
+          list[i].classList.remove("active");
+       }
+      for(var z = 0; z < textBoxs.length; z++){
+          textBoxs[z].classList.remove("show");
+       }
+      
+        if(target.tagName = 'LI'){
+          target.parentElement.classList.add("active");
+          if(target.parentElement.id == "box_item_first"){
+              text_box_first.classList.add("show");
+          }
+          if(target.parentElement.id == "box_item_second"){
+              text_box_second.classList.add("show");
+          }
+          if(target.parentElement.id == "box_item_third"){
+              text_box_third.classList.add("show");
+          }
+        }
+  })
+})();
