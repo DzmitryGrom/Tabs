555a9006edf16417489d163044d13e5319dd3592
DzmitryGrom <Shelegeykodmitriy@mail.ru>
2016-05-16 21:42:10 +0300

    change js, and add drop-down

--- myjs.js
+++ myjs.js
@@ -1,4 +1,4 @@
-(function(){
+/*(function(){
   var captionItem = document.querySelectorAll('li');
   for(var y = 0; y < captionItem.length; y++){
     captionItem[y].addEventListener('click', function(event){
@@ -27,4 +27,45 @@
           }
     })
   }  
+})();
+*/
+(function(){
+    var tabs = document.querySelectorAll('.container-caption-item'),
+    arrows = document.querySelectorAll('i'),
+    dropDownList = document.querySelectorAll('.container-caption-item_dropDown-list'),
+    containers = document.querySelectorAll('.container-caption-text');
+    function handelTabChange(event){
+        
+        for(var i = 0; i < tabs.length; i++){
+            if(event.target.tagName === "LI"){
+                tabs[i].classList.remove('active');
+              }
+        }
+        if(event.target.tagName == 'LI'){
+          event.target.classList.add('active');
+          for(var i = 0; i < dropDownList.length; i++){
+          dropDownList[i].classList.remove('show');
+        }
+          for(var i = 0; i < containers.length; i++){
+            containers[i].classList.remove('show');
+          }
+          document.querySelector('.container-caption-text[data-url='+event.target.getAttribute('data-url')+']').classList.add('show');
+        }
+    }
+    for(var i = 0; i < tabs.length; i++){
+      tabs[i].addEventListener('click', handelTabChange);
+    }
+       
+        
+    function dropDown(event){
+       if(event.target.parentElement.classList[1] == 'active'){ 
+          event.target.parentElement.childNodes[3].classList.add('show');
+          console.log(event.target.parentElement.classList[1])
+       }
+    }   
+        
+        for(var i = 0; i < arrows.length; i++){
+          arrows[i].addEventListener('click', dropDown);
+
+        } 
 })();
